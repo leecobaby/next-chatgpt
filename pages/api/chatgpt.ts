daf9482e8d60a0988f2b6d9e7fb4ce75831d6030
Leeco <leeco19@qq.com>
2022-12-08 14:38:25 +0800

    fix fetch polyfills bug in nextjs/vercel

--- pages/api/chatgpt.ts
+++ pages/api/chatgpt.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { ChatGPTAPI } from './chatgpt/index'
+import { ChatGPTAPI } from 'chatgpt'
 import dotenv from 'dotenv-safe'
 dotenv.config()
 
